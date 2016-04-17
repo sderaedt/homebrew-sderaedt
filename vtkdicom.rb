9d25160718de0b8a79524a798d2a9ae0ff0dba89
Sepp de Raedt <sderaedt@gmail.com>
2016-04-17 07:12:43 +0200

    Update vtkdicom

--- vtkdicom.rb
+++ vtkdicom.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vtkdicom < Formula
   homepage 'http://dgobbi.github.io/vtk-dicom/'
-  url 'https://github.com/dgobbi/vtk-dicom/archive/v0.6.6.tar.gz'
-  sha1 '5b95396c197cdf4f9163080fa72ef5c36a31f807'
+  url 'https://github.com/dgobbi/vtk-dicom/archive/v0.6.7.tar.gz'
+  sha1 ''
   
   head 'git://github.com/dgobbi/vtk-dicom.git'
 
